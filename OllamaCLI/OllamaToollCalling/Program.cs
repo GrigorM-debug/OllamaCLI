b28e3336d67b09f9d2d04d5b9031b165bbe49834
Grigor Jivkov Marinov <grigormarinov761@gmail.com>
2025-07-19 17:11:10 +0300

    Added console app to test Tool Calling with Ollama qwen2.5:0.5b  model

---
+++ OllamaCLI/OllamaToollCalling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography;
+using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+internal class Program
+{
+    public static async Task Main()
+    {
+        
+        string input = "Should I wear a coat today?";
+
+        IChatClient client = new OllamaApiClient(
+            new Uri("http://localhost:11434/"), "qwen2.5:0.5b");
+
+        client = ChatClientBuilderChatClientExtensions
+            .AsBuilder(client)
+            .UseFunctionInvocation()
+            .Build();
+
+        ChatOptions options = new()
+        {
+            Tools = [AIFunctionFactory.Create(GetCurrentWeather)]
+        };
+
+        var reponse = client.GetStreamingResponseAsync(input, options);
+
+        await foreach (var update in reponse)
+        {
+            Console.Write(update.Text);
+        }
+    }
+
+    private static string GetCurrentWeather()
+    {
+        if (Random.Shared.NextDouble() > 0.5)
+        {
+            return "It's sunny and warm today.";
+        }
+        else
+        {
+            return "It's raining today.";
+        }
+    }
+}
